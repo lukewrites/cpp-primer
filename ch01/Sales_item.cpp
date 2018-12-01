e4832f1654810e2996e9f3e5f18a37e8e36a6009
Luke Petschauer <luke@lukewrites.com>
2018-12-01 12:17:39 -0800

    §1.6 Sales_item.cpp file updated

--- ch01/Sales_item.cpp
+++ ch01/Sales_item.cpp
@@ -3,10 +3,27 @@
 
 int main(int argc, char const *argv[])
 {
-    Sales_item book;
-    // read ISB, numbers of copies sold, and sales price
-    std::cin >> book;
-    // write ISBN, numbr of copies sold, and avg price
-    std::cout << book << std::endl;
+    Sales_item total; // var to hold data for next transaction
+    // read first transac and ensure there are data to process
+    if (std::cin >> total){
+        Sales_item trans; // var to hold running sum
+        // read and process remaining tranactions
+
+        while(std::cin >> trans){
+            // if we're still processing the same book
+            if (total.isbn() == trans.isbn()) {
+                total += trans;  // update running total
+            }
+            else {
+                std::cout << total << std::endl;
+                total = trans;  // `total` now refers to the next/new book.
+            }
+        }
+        std::cout << total << std::endl; // print last transaction
+    } else {
+        // no input! warn the user
+        std::cerr << "No data?!?" << std::endl;
+        return -1;
+    }
     return 0;
 }
